f7c8d0075b6e25610a1050f73d7493cb7815f309
Roberto Nogueira <robertondc@gmail.com>
2016-02-23 22:53:27 -0300

    classifica acessos com o csv

---
+++ dados.py
@@ -0,0 +1,17 @@
+import csv
+
+def carregar_acessos():
+	X = []
+	Y = []
+
+	arquivo = open('acesso.csv','rb')
+	leitor = csv.reader(arquivo)
+
+	leitor.next() #pula header
+
+	for home,funciona,contato,comprou in leitor:
+		dado = [int(home),int(funciona),int(contato)]
+		X.append(dado)
+		Y.append(int(comprou))
+
+	return X, Y 
