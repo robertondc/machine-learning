98172fef128b869ab107197d9008671249143206
Roberto Nogueira <robertondc@gmail.com>
2016-02-27 22:30:08 -0300

    adicionar modelo de classificacao AdaBoost

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -18,29 +18,43 @@ tamanho_de_treino = porcentagem_de_treino * len(Y)
 tamanho_de_teste = len(Y) - tamanho_de_treino
 
 treino_dados = X[:tamanho_de_treino]
-treino_marcacoes = Y[:tamanho_de_treino]
+treino_marcadores = Y[:tamanho_de_treino]
 
 teste_dados = X[-tamanho_de_teste:]
-teste_marcacoes = Y[-tamanho_de_teste:]
+teste_marcadores = Y[-tamanho_de_teste:]
+
+def fit_and_predict(nome,modelo, treino_dados, treino_marcadores, teste_dados, teste_marcadores):
+	modelo.fit(treino_dados,treino_marcadores)
+
+	resultado = modelo.predict(teste_dados)
+	acertos = (resultado == teste_marcadores)
+
+	total_de_acertos = sum(acertos) #true e false somam como 0 e 1
+	total_de_elementos = len(teste_dados)
+	taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+	
+	msg = "Taxa de acerto do {0}: {1}".format(nome, taxa_de_acerto)	
+	print(msg)
+	return taxa_de_acerto
 
 from sklearn.naive_bayes import MultinomialNB
-modelo = MultinomialNB()
-modelo.fit(treino_dados,treino_marcacoes)
+modelo_multinomial = MultinomialNB()
+resultado_multinomial = fit_and_predict("MultinomialNB", modelo_multinomial, treino_dados, treino_marcadores, teste_dados, teste_marcadores)
 
-resultado = modelo.predict(teste_dados)
-acertos = (resultado == teste_marcacoes)
+from sklearn.ensemble import AdaBoostClassifier
+modelo_adaboost = AdaBoostClassifier()
+resultado_adaboost = fit_and_predict("AdaBoostClassifier", modelo_adaboost, treino_dados, treino_marcadores, teste_dados, teste_marcadores)
 
-total_de_acertos = sum(acertos) #true e false somam como 0 e 1
-total_de_elementos = len(teste_dados)
-taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+if resultado_multinomial > resultado_adaboost:
+	vencedor = modelo_multinomial
+else:
+	vencedor = modelo_adaboost
 
-print(taxa_de_acerto)
-print(total_de_elementos)
 
 #eficacia do algoritimo via chute
 
-acerto_base = max(Counter(teste_marcacoes).itervalues())
-taxa_de_acerto_base = 100.0 * acerto_base / total_de_elementos
+acerto_base = max(Counter(teste_marcadores).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_dados)
 print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 print("Total de testes: %d" % len(teste_dados))
 
